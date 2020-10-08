2d780f20fb73917be4b0040e7281338c41c8682e
buscaroli <m.buscaroli@outlook.com>
2020-10-08 17:31:43 +0100

    Worked on the Header, added Club logos.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
 import logo from '../assets/images/diamond.png'
+import enci from '../assets/images/logo-enci-orange.png'
+import fci from '../assets/images/logo-fci-orange.png'
 
 const Header = () => {
     return (
         <div className='header'>
             <Navbar />
                 <div className='header__logo'>
-                    <img src={logo} alt='diamond logo'/>
+                    <img src={logo} alt='logo diamante'/>
+                </div>
+                <div className='header__clubs'>
+                    <img className='header__clubs--fci' src={fci} alt='logo fci'/>
+                    <img className='header__clubs--enci' src={enci} alt='logo enci'/>
                 </div>
                 <h1 className='header__title'>
                     La Corte dei Grandi
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { IconContext } from 'react-icons'
+import { FaFacebook } from 'react-icons/fa'
 
 function Navbar() {
     return (
@@ -9,6 +11,15 @@ function Navbar() {
                 <li className='navbar__item navbar__items--2'><Link to='/about'>Chi Siamo</Link></li>
                 <li className='navbar__item navbar__items--3'><Link to='/pets'>Cuccioli</Link></li>
                 <li className='navbar__item navbar__items--4'><Link to='/contacts'>Contatti</Link></li>
+                <li className='navbar__item navbar__items--5'>
+                    <a href='https://www.facebook.com/lacortedeigrandi/' target='_blank'>
+                        <IconContext.Provider value={{style: {color: 'dodgerblue', fontSize: '4rem'}}}>
+                            <div>
+                                <FaFacebook />
+                            </div> 
+                        </IconContext.Provider>
+                    </a>
+                </li>
             </ul>
         </div>
     )
