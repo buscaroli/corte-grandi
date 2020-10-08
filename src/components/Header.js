c9c0417d6d370ef1326b48785975c8e21382de7b
buscaroli <m.buscaroli@outlook.com>
2020-10-08 13:47:22 +0100

    Worked on header and navbar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import logo from '../assets/images/diamond.png'
 
 const Header = () => {
     return (
         <div className='header'>
             <Navbar />
-            <h1 className='header__title'>
-                La Corte dei Grandi
-            </h1>
-            <h2 className='header__subtitle'>
-                Allevamento cinofilo per la selezione del <span>Cavalier King Charles Spaniel</span>
-            </h2>      
+                <div className='header__logo'>
+                    <img src={logo} alt='diamond logo'/>
+                </div>
+                <h1 className='header__title'>
+                    La Corte dei Grandi
+                </h1>
+                <h2 className='header__subtitle'>
+                    Allevamento cinofilo per la selezione del <span>Cavalier King Charles Spaniel</span>
+                </h2>    
         </div>
     )
 }
